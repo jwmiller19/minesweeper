658c435e7fff5d056d84c134180b4f5679dca09f
Joe Miller <joewmiller19@gmail.com>
2021-01-06 16:49:34 -0500

    Add basic board class with bomb placing
    Added a simple Board class to represent the minesweeper game board.
    Also added logic for placing random bombs on board creation.

---
+++ board.rb
@@ -0,0 +1,28 @@
+require_relative "tile"
+
+class Board
+  def initialize
+    @grid = new_grid
+  end
+
+  # Create a new 9x9 grid of tiles with 10 random tiles containing bombs
+  def new_grid
+    indices = (0...81).to_a
+    bomb_positions = indices.sample(10)
+
+    i = -1
+    bomb_count = 0
+
+    Array.new(9) do
+      Array.new(9) do
+        i += 1
+        if bomb_count < 10 && bomb_positions.include?(i)
+          bomb_count += 1
+          Tile.new(true)
+        else
+          Tile.new(false)
+        end
+      end
+    end
+  end
+end
