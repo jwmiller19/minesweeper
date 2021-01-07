71ee35d5279646c340818a37ba5dc977fbdb8c5f
Joe Miller <joewmiller19@gmail.com>
2021-01-06 19:05:48 -0500

    Add functionality for checking neighboring tiles

--- tile.rb
+++ tile.rb
@@ -8,6 +8,7 @@ class Tile
     @revealed = false
   end
 
+  # Omit the board to avoid recursive output for inspect
   def inspect
     "#{self.class.name}:#{self.object_id} " +
     "@bombed=#{@bombed}, @flagged=#{@flagged}, @revealed=#{@revealed}"
@@ -31,5 +32,52 @@ class Tile
 
   def reveal
     @revealed = true unless revealed?
+    return if bombed?
   end
+
+  # Return an array of all tiles neighboring self (including diagonals)
+  def neighbors
+    neighbor_tiles = []
+    grid = @board.grid
+
+    all_tiles = grid.flatten
+    self_index = all_tiles.index(self)
+    self_row = self_index / grid.length
+    self_col = self_index % grid.length
+    self_pos = [self_row, self_col]
+
+    grid.each_index do |row|
+      grid[row].each_index do |col|
+        pos = [row, col]
+        neighbor_tiles << grid[row][col] if neighbor?(self_pos, pos)
+      end
+    end
+
+    neighbor_tiles
+  end
+
+  # Check if other_pos is a neighboring position of self_pos
+  def neighbor?(self_pos, other_pos)
+    self_row, self_col = self_pos
+    row, col = other_pos
+
+    same_row = row == self_row
+    same_col = col == self_col
+
+    left_or_right = col == self_col - 1 || col == self_col + 1
+    up_or_down = row == self_row - 1 || row == self_row + 1
+
+    diagonal = left_or_right && up_or_down
+
+    if (same_row && left_or_right) || (same_col && up_or_down) || diagonal
+      true
+    else
+      false
+    end
+  end
+
+  def neighbor_bomb_count
+    neighbors.count(&:bombed?)
+  end
+
 end
